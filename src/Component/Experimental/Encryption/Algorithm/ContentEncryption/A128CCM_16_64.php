ec22179496e0b829fdb0460d890426572d405775
Spomky <florent@morselli.fr>
2018-05-15 22:34:21 +0200

    Some experimental algorithms added

---
+++ src/Component/Experimental/Encryption/Algorithm/ContentEncryption/A128CCM_16_64.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2018 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace Jose\Component\Experimental\Encryption\Algorithm\ContentEncryption;
+
+final class A128CCM_16_64 extends AESCCM
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getCEKSize(): int
+    {
+        return 128;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getMode(): string
+    {
+        return 'aes-128-ccm';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function name(): string
+    {
+        return 'A128CCM-16-64';
+    }
+
+    protected function getTagLength(): int
+    {
+        return 8;
+    }
+
+    public function getIVSize(): int
+    {
+        return 13;
+    }
+}
---
+++ src/Component/Experimental/Encryption/Algorithm/KeyEncryption/A192CTR.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2018 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace Jose\Component\Experimental\Encryption\Algorithm\KeyEncryption;
+
+final class A192CTR extends AESCTR
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function name(): string
+    {
+        return 'A192CTR';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getMode(): string
+    {
+        return 'aes-192-ctr';
+    }
+}
