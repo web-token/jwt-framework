8066ef63f542bc0bd8b9e6c4f285cc8109a46105
Spomky <florent@morselli.fr>
2018-06-03 18:14:54 +0200

    Bug fixed
    Routes with :: does not work properly with Symfony 3.
    x if he controller is a service

--- src/Bundle/JoseFramework/Routing/JWKSetLoader.php
+++ src/Bundle/JoseFramework/Routing/JWKSetLoader.php
@@ -39,7 +39,7 @@ class JWKSetLoader implements LoaderInterface
      */
     public function add(string $pattern, string $name)
     {
-        $controller_id = sprintf('%s::getAction', $name);
+        $controller_id = sprintf('%s:getAction', $name);
         $defaults = ['_controller' => $controller_id];
         $route = new Route($pattern, $defaults);
         $this->routes->add(sprintf('jwkset_%s', $name), $route);
