62307dfff17e42be4479a46f4880c8014098c0f5
Florent Morselli <Spomky@users.noreply.github.com>
2019-02-07 11:01:52 +0100

    Update KeyAnalyzerCompilerPass.php

--- src/Bundle/JoseFramework/DependencyInjection/Compiler/KeyAnalyzerCompilerPass.php
+++ src/Bundle/JoseFramework/DependencyInjection/Compiler/KeyAnalyzerCompilerPass.php
@@ -20,6 +20,9 @@ use Symfony\Component\DependencyInjection\Reference;
 
 final class KeyAnalyzerCompilerPass implements CompilerPassInterface
 {
+    /**
+     * {@inheritdoc}
+     */
     public function process(ContainerBuilder $container)
     {
         if (!$container->hasDefinition(KeyAnalyzerManager::class)) {
