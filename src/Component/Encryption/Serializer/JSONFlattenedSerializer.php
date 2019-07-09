51371f377aaeb641fc68f7b02f841a03aaac3056
Maarten de Boer <maarten@cloudstek.nl>
2019-07-09 10:59:32 +0200

    Fix JWE serialiser crashing on decoding invalid JWE
    The JWE serialiser crashes when attempting to decode invalid data or even a JWS. The serialiser returns null but checkData does not accept a null value and throws an exception.

--- src/Component/Encryption/Serializer/JSONFlattenedSerializer.php
+++ src/Component/Encryption/Serializer/JSONFlattenedSerializer.php
@@ -88,9 +88,9 @@ final class JSONFlattenedSerializer implements JWESerializer
         );
     }
 
-    private function checkData(array $data): void
+    private function checkData(?array $data): void
     {
-        if (!isset($data['ciphertext']) || isset($data['recipients'])) {
+        if ($data === null || !isset($data['ciphertext']) || isset($data['recipients'])) {
             throw new InvalidArgumentException('Unsupported input.');
         }
     }
