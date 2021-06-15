b40f1219d6e4a30e9e37825133c97eea414f4c8e
Martin Herndl <martin@herndl.org>
2021-06-15 21:04:56 +0200

    Fix Symfony 5.1 deprecation
    This adds a conditional to use service() if existing with fallback to ref() otherwise to support Symfony 4.4.

--- src/Bundle/JoseFramework/Resources/config/jku_source.php
+++ src/Bundle/JoseFramework/Resources/config/jku_source.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 use Jose\Component\KeyManagement;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return function (ContainerConfigurator $container): void {
     $container = $container->services()->defaults()
@@ -22,19 +24,25 @@ return function (ContainerConfigurator $container): void {
         ->autowire()
     ;
 
+    $serviceClosure = static function (string $serviceId): ReferenceConfigurator {
+        return function_exists('Symfony\Component\DependencyInjection\Loader\Configurator\service')
+            ? service($serviceId)
+            : ref($serviceId);
+    };
+
     $container->set(KeyManagement\JKUFactory::class)
         ->public()
         ->args([
-            ref('jose.http_client'),
-            ref('jose.request_factory'),
+            ($serviceClosure)('jose.http_client'),
+            ($serviceClosure)('jose.request_factory'),
         ])
     ;
 
     $container->set(KeyManagement\X5UFactory::class)
         ->public()
         ->args([
-            ref('jose.http_client'),
-            ref('jose.request_factory'),
+            ($serviceClosure)('jose.http_client'),
+            ($serviceClosure)('jose.request_factory'),
         ])
     ;
 };
