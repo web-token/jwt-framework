c0cc59632108b3afcf25dac1e8b01f66752062c5
Florent Morselli <contact@spomky-labs.com>
2022-06-29 18:29:26 +0200

    exp, iat and nbf claim checkers can also be header checkers

--- src/Bundle/JoseFramework/Resources/config/checkers.php
+++ src/Bundle/JoseFramework/Resources/config/checkers.php
@@ -38,16 +38,25 @@ return function (ContainerConfigurator $container): void {
         ->tag('jose.checker.claim', [
             'alias' => 'exp',
         ])
+        ->tag('jose.checker.header', [
+            'alias' => 'exp',
+        ])
     ;
 
     $container->set(IssuedAtChecker::class)
         ->tag('jose.checker.claim', [
             'alias' => 'iat',
         ])
+        ->tag('jose.checker.header', [
+            'alias' => 'iat',
+        ])
     ;
 
     $container->set(NotBeforeChecker::class)
         ->tag('jose.checker.claim', [
+            'alias' => 'nbf',
+        ])
+        ->tag('jose.checker.header', [
             'alias' => 'nbf',
         ])
     ;
