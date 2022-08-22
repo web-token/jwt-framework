0ffa1827537db58637a8d4c81d507a815ceda0f4
Florent Morselli <contact@spomky-labs.com>
2022-08-22 17:34:11 +0200

    Rebase

--- src/Component/Console/OctKeyGeneratorCommand.php
+++ src/Component/Console/OctKeyGeneratorCommand.php
@@ -6,14 +6,17 @@ namespace Jose\Component\Console;
 
 use InvalidArgumentException;
 use Jose\Component\KeyManagement\JWKFactory;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'key:generate:oct',
+    description: 'Generate an octet key (JWK format)',
+)]
 final class OctKeyGeneratorCommand extends GeneratorCommand
 {
-    protected static $defaultName = 'key:generate:oct';
-
     protected function configure(): void
     {
         parent::configure();
