a6b7bcce459a6478fc903beb66f3477fb1e366c9
Florent Morselli <contact@spomky-labs.com>
2022-08-22 18:01:17 +0200

    Rebased

--- src/Component/Encryption/Algorithm/KeyEncryption/KeyWrapping.php
+++ src/Component/Encryption/Algorithm/KeyEncryption/KeyWrapping.php
@@ -12,19 +12,19 @@ interface KeyWrapping extends KeyEncryptionAlgorithm
     /**
      * Encrypt the CEK.
      *
-     * @param JWK $key The key used to wrap the CEK
-     * @param string $cek The CEK to encrypt
-     * @param array $completeHeader The complete header of the JWT
-     * @param array $additionalHeader The complete header of the JWT
+     * @param JWK    $key              The key used to wrap the CEK
+     * @param string $cek              The CEK to encrypt
+     * @param array  $completeHeader   The complete header of the JWT
+     * @param array  $additionalHeader The complete header of the JWT
      */
     public function wrapKey(JWK $key, string $cek, array $completeHeader, array &$additionalHeader): string;
 
     /**
      * Decrypt de CEK.
      *
-     * @param JWK $key The key used to wrap the CEK
-     * @param string $encrypted_cek The CEK to decrypt
-     * @param array $completeHeader The complete header of the JWT
+     * @param JWK    $key            The key used to wrap the CEK
+     * @param string $encrypted_cek  The CEK to decrypt
+     * @param array  $completeHeader The complete header of the JWT
      */
     public function unwrapKey(JWK $key, string $encrypted_cek, array $completeHeader): string;
 }
