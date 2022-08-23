103a828d2b544d58b85f551b48e53a14cd9df6d1
Florent Morselli <contact@spomky-labs.com>
2022-08-23 19:02:46 +0200

    Blake2b added as an experimental algorithm

---
+++ tests/SignatureAlgorithm/Experimental/Blake2bTest.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jose\Tests\Component\Signature\Algorithm;
+
+use InvalidArgumentException;
+use Jose\Component\Core\JWK;
+use Jose\Component\Signature\Algorithm\Blake2b;
+use ParagonIE\ConstantTime\Base64UrlSafe;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+final class Blake2bTest extends TestCase
+{
+    private const KEY_ONE = 'GOu4rLyVCBxmxP-sbniU68ojAja5PkRdvv7vNvBCqDQ';
+
+    private const KEY_TWO = 'Pu7gywseH-R5HLIWnMll4rEg1ltjUPq_P9WwEzAsAb8';
+
+    private const CONTENTS = 'test';
+
+    private const EXPECTED_HASH_WITH_KEY_ONE = '_TG5kmkav_YGl3I9uQiv4cm1VN6Q0zPCom4G7-p74JU';
+
+    private const SHORT_KEY = 'PIBQuM5PopdMxtmTWmyvNA';
+
+    private JWK $keyOne;
+
+    private JWK $keyTwo;
+
+    private string $expectedHashWithKeyOne;
+
+    /**
+     * @before
+     */
+    public function initializeKey(): void
+    {
+        $this->keyOne = new JWK([
+            'kty' => 'oct',
+            'k' => self::KEY_ONE,
+        ]);
+        $this->keyTwo = new JWK([
+            'kty' => 'oct',
+            'k' => self::KEY_TWO,
+        ]);
+        $this->expectedHashWithKeyOne = Base64UrlSafe::decode(self::EXPECTED_HASH_WITH_KEY_ONE);
+    }
+
+    /**
+     * @test
+     */
+    public function algorithmIdMustBeCorrect(): void
+    {
+        $algorithm = new Blake2b();
+
+        static::assertSame('BLAKE2B', $algorithm->name());
+    }
+
+    /**
+     * @test
+     */
+    public function generatedSignatureMustBeSuccessfullyVerified(): void
+    {
+        $algorithm = new Blake2b();
+
+        static::assertTrue(hash_equals($this->expectedHashWithKeyOne, $algorithm->hash($this->keyOne, self::CONTENTS)));
+        static::assertTrue($algorithm->verify($this->keyOne, self::CONTENTS, $this->expectedHashWithKeyOne));
+    }
+
+    /**
+     * @test
+     */
+    public function signShouldRejectShortKeys(): void
+    {
+        $algorithm = new Blake2b();
+        $key = new JWK([
+            'kty' => 'oct',
+            'k' => self::SHORT_KEY,
+        ]);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Key provided is shorter than 256 bits.');
+
+        $algorithm->hash($key, self::CONTENTS);
+    }
+
+    /**
+     * @test
+     */
+    public function verifyShouldReturnFalseWhenExpectedHashWasNotCreatedWithSameInformation(): void
+    {
+        $algorithm = new Blake2b();
+
+        static::assertFalse(
+            hash_equals($this->expectedHashWithKeyOne, $algorithm->hash($this->keyTwo, self::CONTENTS))
+        );
+        static::assertFalse($algorithm->verify($this->keyTwo, self::CONTENTS, $this->expectedHashWithKeyOne));
+    }
+}
