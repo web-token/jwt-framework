050c8b371b84930dd4daba8f84aff273dc6df5f1
Florent Morselli <contact@spomky-labs.com>
2023-03-11 15:35:48 +0100

    Keyset as string or integer (#437)

--- rector.php
+++ rector.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
@@ -14,22 +13,21 @@ use Rector\Symfony\Set\SymfonyLevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $config): void {
-    $config->sets([
-        SetList::DEAD_CODE,
-        LevelSetList::UP_TO_PHP_81,
-        SymfonyLevelSetList::UP_TO_SYMFONY_54,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        SymfonySetList::SYMFONY_STRICT,
-        DoctrineSetList::DOCTRINE_CODE_QUALITY,
-        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-        PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD,
-        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
-        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        PHPUnitSetList::PHPUNIT_EXCEPTION,
-        PHPUnitSetList::REMOVE_MOCKS,
-        PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER,
-    ]);
+    $config->import(SetList::DEAD_CODE);
+    $config->import(LevelSetList::UP_TO_PHP_81);
+    $config->import(SymfonyLevelSetList::UP_TO_SYMFONY_54);
+    $config->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+    $config->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
+    $config->import(SymfonySetList::SYMFONY_STRICT);
+    $config->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
+    $config->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
+    $config->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
+    $config->import(PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD);
+    $config->import(PHPUnitLevelSetList::UP_TO_PHPUNIT_90);
+    $config->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
+    $config->import(PHPUnitSetList::PHPUNIT_EXCEPTION);
+    $config->import(PHPUnitSetList::REMOVE_MOCKS);
+    $config->import(PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER);
     $config->parallel();
     $config->paths([__DIR__ . '/src', __DIR__ . '/performance', __DIR__ . '/tests']);
     $config->skip([
@@ -40,7 +38,4 @@ return static function (RectorConfig $config): void {
     $config->phpVersion(PhpVersion::PHP_81);
     $config->importNames();
     $config->importShortClasses();
-
-    $services = $config->services();
-    $services->set(TypedPropertyRector::class);
 };
