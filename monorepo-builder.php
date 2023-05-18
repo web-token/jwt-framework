77867665ef588c2846863dde955a73ef9c6eef97
Florent Morselli <florent.morselli@spomky-labs.com>
2023-05-18 15:21:23 +0200

    prepare release

---
+++ monorepo-builder.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\MonorepoBuilder\Config\MBConfig;
+
+return static function (MBConfig $mbConfig): void {
+    $mbConfig->packageDirectories([__DIR__ . '/src']);
+};
---
+++ packages/second-package/src/SecondClass.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YourMonorepo\SecondPackage;
+
+final class SecondClass
+{
+}
