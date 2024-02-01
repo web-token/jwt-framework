a916bd7e5f2016484fdb6251e55cadd7de777004
Florent Morselli <florent.morselli@spomky-labs.com>
2024-02-01 19:45:27 +0100

    Allows Symfony Http Client

--- src/Library/KeyManagement/UrlKeySetFactory.php
+++ src/Library/KeyManagement/UrlKeySetFactory.php
@@ -7,6 +7,7 @@ namespace Jose\Component\KeyManagement;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use RuntimeException;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
  * @see \Jose\Tests\Component\KeyManagement\UrlKeySetFactoryTest
@@ -14,12 +15,49 @@ use RuntimeException;
 abstract class UrlKeySetFactory
 {
     public function __construct(
-        private readonly ClientInterface $client,
-        private readonly RequestFactoryInterface $requestFactory
+        private readonly ClientInterface|HttpClientInterface $client,
+        private readonly null|RequestFactoryInterface $requestFactory = null
     ) {
+        if ($this->client instanceof ClientInterface) {
+            trigger_deprecation(
+                'web-token/jwt-library',
+                '3.3',
+                'Using "%s" with an instance of "%s" is deprecated, use "%s" instead.',
+                self::class,
+                ClientInterface::class,
+                HttpClientInterface::class
+            );
+        }
+        if (! $this->client instanceof HttpClientInterface && $this->requestFactory === null) {
+            throw new RuntimeException(qprintf(
+                'The request factory must be provided when using an instance of "%s" as client.',
+                ClientInterface::class
+            ));
+        }
     }
 
     protected function getContent(string $url, array $header = []): string
+    {
+        if ($this->client instanceof HttpClientInterface) {
+            return $this->sendSymfonyRequest($url, $header);
+        }
+        return $this->sendPsrRequest($url, $header);
+    }
+
+    private function sendSymfonyRequest(string $url, array $header = []): string
+    {
+        $response = $this->client->request('GET', $url, [
+            'headers' => $header,
+        ]);
+
+        if ($response->getStatusCode() >= 400) {
+            throw new RuntimeException('Unable to get the key set.', $response->getStatusCode());
+        }
+
+        return $response->getContent();
+    }
+
+    private function sendPsrRequest(string $url, array $header = []): string
     {
         $request = $this->requestFactory->createRequest('GET', $url);
         foreach ($header as $k => $v) {
