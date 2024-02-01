d6144d297be9fb48c727e19fab4754abfa072af0
Florent Morselli <florent.morselli@spomky-labs.com>
2024-02-01 19:51:39 +0100

    Allows Symfony Http Client

--- src/Library/KeyManagement/UrlKeySetFactory.php
+++ src/Library/KeyManagement/UrlKeySetFactory.php
@@ -8,6 +8,7 @@ use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use RuntimeException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use function assert;
 
 /**
  * @see \Jose\Tests\Component\KeyManagement\UrlKeySetFactoryTest
