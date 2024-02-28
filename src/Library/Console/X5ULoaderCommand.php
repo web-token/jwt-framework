71dc0537e8ec66455d184e764510c659f2e4d3a7
Stephan Wentz <swentz@brainbits.net>
2024-02-28 08:46:05 +0100

    Add missing AsCommand attributes for commands

--- src/Library/Console/X5ULoaderCommand.php
+++ src/Library/Console/X5ULoaderCommand.php
@@ -6,6 +6,7 @@ namespace Jose\Component\Console;
 
 use InvalidArgumentException;
 use Jose\Component\KeyManagement\X5UFactory;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
