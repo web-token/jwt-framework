1dbef13afb91a576d5ce431e5f9570183b19c0dd
Stephan Wentz <stephan@wentz.it>
2024-02-28 10:04:35 +0100

    Add missing AsCommand attributes for commands (#532)
    Co-authored-by: Stephan Wentz <swentz@brainbits.net>

--- src/Library/Console/NoneKeyGeneratorCommand.php
+++ src/Library/Console/NoneKeyGeneratorCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Jose\Component\Console;
 
 use Jose\Component\KeyManagement\JWKFactory;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
