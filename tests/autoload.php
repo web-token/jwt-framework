dd3b4f84c3b9430bb6d9cda80ed65b2a014ce2ea
Florent Morselli <florent.morselli@spomky-labs.com>
2024-06-16 13:20:01 +0200

    Refine autoload path in tests
    The autoload path in 'tests/autoload.php' has been updated to ensure consistency and proper functioning. The refinement adds necessary spaces for optimal readability and maintainability while reinforcing the correct pathing for autoloading.

--- tests/autoload.php
+++ tests/autoload.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 use Symfony\Component\ErrorHandler\ErrorHandler;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 set_exception_handler([new ErrorHandler(), 'handleException']);
